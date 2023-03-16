55538a9007e77a7680e6bfa6a0601de91c83935c
Camila Martinez <camilamarb98@gmail.com>
2023-03-16 18:23:29 +0100

    Add Cloudfare connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      config: { applyBaseStyles: false },
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: {
+      applyBaseStyles: false
+    }
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
