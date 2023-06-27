e28c3838c5a5925c705a687bdf3cd8cca05b903f
Camila Martinez <camilamarb98@gmail.com>
2023-06-27 13:25:10 +0200

    Update Root file, styles

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { PageLayout } from '@primer/react'
+import Footer from './components/Footer'
+import Pane from './components/Pane'
 
 export default function Root() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <PageLayout>
+        <PageLayout.Header divider="line">
+          <Navbar />
+        </PageLayout.Header>
+        <PageLayout.Content>
+          <Outlet />
+        </PageLayout.Content>
+        <PageLayout.Pane position={'start'}>
+          <Pane />
+        </PageLayout.Pane>
+        <PageLayout.Footer divider="line">
+          <Footer />
+        </PageLayout.Footer>
+      </PageLayout>
     </>
-  );
+  )
 }
