435911a075bcb75a82dd57e9729b1c4e527ac855
Camila Martinez <camilamarb98@gmail.com>
2023-07-12 13:16:41 +0200

    Update Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,11 @@ import { Button, Pagehead } from '@primer/react'
 export default function Home() {
   return (
     <>
-      <div>Hey, I'm Camila 👋 and I'm a Web Developer. Berlin</div>
+      <div>
+        <Pagehead>
+          Hey, I'm Camila 👋 and I'm a Web Developer. 📍 Berlin
+        </Pagehead>
+      </div>
       <Button>this is a button</Button>
     </>
   )
