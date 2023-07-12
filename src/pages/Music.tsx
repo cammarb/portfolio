f38f6bbe17ba636e26f7bd095a8426bb6d56993a
Camila Martinez <camilamarb98@gmail.com>
2023-07-12 13:15:54 +0200

    add Music Page

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,7 +1,55 @@
+import { Link } from 'react-router-dom'
+import image from '../assets/IMG_0199.png'
+import { ActionList, Avatar, Box } from '@primer/react'
+import { BsApple, BsSpotify, BsYoutube } from 'react-icons/bs'
+import { FaSoundcloud } from 'react-icons/fa'
+
 export default function Music() {
   return (
     <>
-      <div>This is the music page</div>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <div>
+          <Avatar src={image} size={150} />
+        </div>
+        <ActionList showDividers={true}>
+          <ActionList.LinkItem
+            as={Link}
+            to={
+              'https://open.spotify.com/track/7rXGDVMlkU27WjlWgcZbnc?si=a14bba72851b4552'
+            }
+          >
+            <ActionList.LeadingVisual>
+              <BsSpotify width="20" />
+            </ActionList.LeadingVisual>
+            "no time" in Spotify
+          </ActionList.LinkItem>
+
+          <ActionList.LinkItem
+            as={Link}
+            to={
+              'https://music.apple.com/us/album/no-time/1557673169?i=1557673170'
+            }
+          >
+            <ActionList.LeadingVisual>
+              <BsApple width="20" />
+            </ActionList.LeadingVisual>
+            "no time" in Apple Music
+          </ActionList.LinkItem>
+
+          <ActionList.LinkItem as={Link} to={'https://youtu.be/VyVytZmtNmA'}>
+            <ActionList.LeadingVisual>
+              <BsYoutube width="20" />
+            </ActionList.LeadingVisual>
+            "no time" Official Music Video
+          </ActionList.LinkItem>
+          <ActionList.LinkItem as={Link} to={'https://soundcloud.com/camcanta'}>
+            <ActionList.LeadingVisual>
+              <FaSoundcloud width="20" sx={{}} />
+            </ActionList.LeadingVisual>
+            Demos and Covers
+          </ActionList.LinkItem>
+        </ActionList>
+      </Box>
     </>
   )
 }
