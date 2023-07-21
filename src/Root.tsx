f99f57af93b6d92a73b33b3a98c310bc0bc31dff
Camila Martinez <camilamarb98@gmail.com>
2023-07-21 14:41:23 +0200

    Update Root

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,24 +3,32 @@ import Navbar from './components/Navbar'
 import { PageLayout } from '@primer/react'
 import Footer from './components/Footer'
 import Pane from './components/Pane'
+import { themeGet } from '@primer/react'
+import styled from 'styled-components'
 
 export default function Root() {
+  const BgColor = styled.div`
+    background-color: ${themeGet('colors.canvas.default')};
+    min-height: 100vh;
+  `
   return (
     <>
-      <PageLayout>
-        <PageLayout.Header divider="line">
-          <Navbar />
-        </PageLayout.Header>
-        <PageLayout.Content>
-          <Outlet />
-        </PageLayout.Content>
-        <PageLayout.Pane position={'start'}>
-          <Pane />
-        </PageLayout.Pane>
-        <PageLayout.Footer divider="line">
-          <Footer />
-        </PageLayout.Footer>
-      </PageLayout>
+      <BgColor className="bg-color">
+        <PageLayout sx={{}}>
+          <PageLayout.Header divider="line">
+            <Navbar />
+          </PageLayout.Header>
+          <PageLayout.Content>
+            <Outlet />
+          </PageLayout.Content>
+          <PageLayout.Pane position={'start'}>
+            <Pane />
+          </PageLayout.Pane>
+          <PageLayout.Footer divider="line">
+            <Footer />
+          </PageLayout.Footer>
+        </PageLayout>
+      </BgColor>
     </>
   )
 }
