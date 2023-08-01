53799ec3abca77a2cc483810a0b86f8d3acf1a1e
Camila Martinez <camilamarb98@gmail.com>
2023-08-01 10:32:33 +0200

    Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,33 @@
-import { CircleOcticon, Header } from '@primer/react'
+import { Link as PrimerLink } from '@primer/react'
 import { Link } from 'react-router-dom'
-import { MarkGithubIcon } from '@primer/octicons-react'
-import { LinkedinPlain } from 'devicons-react'
+import { styled } from 'styled-components'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export default function Navbar() {
+  const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+  `
+  const LinksContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  `
   return (
     <>
-      <Header sx={{ mb: 2 }}>
-        <Header.Item>
-          <Link to="https://github.com/cammarb">
-            <CircleOcticon icon={MarkGithubIcon} size={16} sx={{ mr: 2 }} />
-          </Link>
-        </Header.Item>
-        <Header.Item>
-          <Link to="https://linkedin.com/cammarb">
-            <LinkedinPlain />
-          </Link>
-        </Header.Item>
-      </Header>
+      <Nav>
+        <PrimerLink as={Link} to={'/'}>
+          cammarb.xyz
+        </PrimerLink>
+        <LinksContainer>
+          <PrimerLink as={Link} to={'https://github.com/cammarb'}>
+            <FaGithub />
+          </PrimerLink>
+          <PrimerLink as={Link} to={'https://linkedin.com/cammarb'}>
+            <FaLinkedin />
+          </PrimerLink>
+        </LinksContainer>
+      </Nav>
     </>
   )
 }
