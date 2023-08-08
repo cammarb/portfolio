e4fa4edf063ad0869e48562ddb7d0b01df59e2bf
Camila Martinez <camilamarb98@gmail.com>
2023-08-08 09:40:18 +0200

    Cleanup pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,137 +1,23 @@
-import { BranchName, Box, Heading, Avatar, Link } from '@primer/react'
-import image from '../assets/profile.png'
-import { LinkIcon } from '@primer/octicons-react'
+import { Link } from 'react-router-dom'
+import Button from '../components/Button'
 
 export default function Home() {
   return (
     <>
-      <Box
-        sx={{
-          display: 'flex',
-          m: 5,
-          alignContent: 'center',
-        }}
-      >
-        <Avatar src={image} size={200} />
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            m: 5,
-          }}
-        >
-          <Heading>Hey, I'm Camila 👋</Heading>
-          <Heading sx={{ fontSize: 4, mb: 3 }}>
-            and I'm a Web Developer.
-          </Heading>
-          <Heading sx={{ fontSize: 2 }}>Currently 📍 Berlin</Heading>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          borderWidth: 1,
-          borderStyle: 'solid',
-          borderColor: 'border.default',
-          py: 3,
-          px: 5,
-        }}
-      >
-        <p>Technologies and programming languages:</p>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '2',
-            pb: 4,
-          }}
-        >
-          <BranchName href="#">Javascript</BranchName>
-          <BranchName href="#">Typescript</BranchName>
-          <BranchName href="#">React</BranchName>
-          <BranchName href="#">Express</BranchName>
-          <BranchName href="#">Python</BranchName>
-          <BranchName href="#">Flask</BranchName>
-          <BranchName href="#">C#</BranchName>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 4,
-          py: 6,
-        }}
-      >
-        <Heading sx={{ fontSize: 4, my: 3 }}>
-          Projects I'm currently working on:
-        </Heading>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '2',
-            p: 5,
-            borderWidth: 1,
-            borderStyle: 'solid',
-            borderColor: 'border.default',
-          }}
-        >
-          <Heading sx={{ fontSize: 2 }}> My portfolio</Heading>
-          <Link
-            href="https://github.com/cammarb/portfolio"
-            sx={{ display: 'flex', alignItems: 'center', gap: 2, my: 2 }}
-          >
-            <LinkIcon />
-            Repository
+      <div id="heading">
+        <h1>Hey, I'm Camila</h1>
+        <p className="text-2xl my-5">
+          I'm a full-stack developer and tech enthusiast. Currently working in
+          IT in Berlin. Sometimes I make music, you can check it out{' '}
+          <Link to={'music'}>here</Link>.
+        </p>
+        <div>
+          <Link to={'projects'}>
+            <Button text={'Check my web projects'} />
           </Link>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              gap: '2',
-            }}
-          >
-            <BranchName href="#">Typescript</BranchName>
-            <BranchName href="#">React + Vite</BranchName>
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '2',
-            p: 5,
-            borderWidth: 1,
-            borderStyle: 'solid',
-            borderColor: 'border.default',
-          }}
-        >
-          <Heading sx={{ fontSize: 2 }}>Defendly</Heading>
-          <Link
-            href="https://defendly.de/"
-            sx={{ display: 'flex', alignItems: 'center', gap: 2, my: 2 }}
-          >
-            <LinkIcon />
-            Landing Page
-          </Link>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              gap: '2',
-            }}
-          >
-            <BranchName href="#">Javascript</BranchName>
-            <BranchName href="#">React + Vite</BranchName>
-            <BranchName href="#">Express JS</BranchName>
-          </Box>
-        </Box>
-        <Link href="/projects" sx={{ textAlign: 'center' }}>
-          See more
-        </Link>
-      </Box>
+          <Link to={''} />
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,65 +1,11 @@
-import { Link } from 'react-router-dom'
 import image from '../assets/no time.jpeg'
-import { ActionList, Avatar, Box } from '@primer/react'
-import { BsApple, BsSpotify, BsYoutube } from 'react-icons/bs'
-import { MdLyrics } from 'react-icons/md'
-import { FaSoundcloud } from 'react-icons/fa'
 
 export default function Music() {
   return (
     <>
-      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-        <div>
-          <Avatar src={image} size={150} />
-        </div>
-        <ActionList showDividers={true}>
-          <ActionList.LinkItem
-            as={Link}
-            to={
-              'https://open.spotify.com/track/7rXGDVMlkU27WjlWgcZbnc?si=a14bba72851b4552'
-            }
-          >
-            <ActionList.LeadingVisual>
-              <BsSpotify width="20" />
-            </ActionList.LeadingVisual>
-            "no time" in Spotify
-          </ActionList.LinkItem>
-
-          <ActionList.LinkItem
-            as={Link}
-            to={
-              'https://music.apple.com/us/album/no-time/1557673169?i=1557673170'
-            }
-          >
-            <ActionList.LeadingVisual>
-              <BsApple width="20" />
-            </ActionList.LeadingVisual>
-            "no time" in Apple Music
-          </ActionList.LinkItem>
-
-          <ActionList.LinkItem as={Link} to={'https://youtu.be/VyVytZmtNmA'}>
-            <ActionList.LeadingVisual>
-              <BsYoutube width="20" />
-            </ActionList.LeadingVisual>
-            "no time" Official Music Video
-          </ActionList.LinkItem>
-          <ActionList.LinkItem as={Link} to={'https://soundcloud.com/camcanta'}>
-            <ActionList.LeadingVisual>
-              <FaSoundcloud width="20" sx={{}} />
-            </ActionList.LeadingVisual>
-            Demos and Covers
-          </ActionList.LinkItem>
-          <ActionList.LinkItem
-            as={Link}
-            to={'https://genius.com/Cam-martinez-no-time-lyrics'}
-          >
-            <ActionList.LeadingVisual>
-              <MdLyrics width="20" sx={{}} />
-            </ActionList.LeadingVisual>
-            Lyrics in Genius
-          </ActionList.LinkItem>
-        </ActionList>
-      </Box>
+      <div className="flex justify-center">
+        <img src={image} className="rounded drop-shadow-md hover:w-full" />
+      </div>
     </>
   )
 }
