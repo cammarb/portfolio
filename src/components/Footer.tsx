35ee6b847241e95df2fcf2bc98d76185e4557c58
Camila Martinez <camilamarb98@gmail.com>
2023-08-10 22:24:49 +0200

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <>
-      <footer className="py-4">
-        <p>Camila Martinez © All rights reserved</p>
+      <footer className="py-4 text-center">
+        <p>© 2023 Camila Martinez. All rights reserved</p>
       </footer>
     </>
   )
