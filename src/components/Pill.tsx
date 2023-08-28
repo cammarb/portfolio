ed657509f4275a2012a2c217e8453c198220f42c
Camila Martinez <camilamarb98@gmail.com>
2023-08-28 02:23:43 +0200

    fix

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,19 +1,13 @@
-import { Component } from 'react'
-
 type Props = {
   language: String
 }
 
-type State = {}
-
-export default class Pill extends Component<Props, State> {
-  state = {}
-
-  render() {
-    return (
-      <div className="w-fit px-2 py-1 rounded bg-indigo-500 bg-opacity-20">
-        <p className="text-indigo-500 text-sm">{this.props.language}</p>
-      </div>
-    )
-  }
+const Pill = ({ language }: Props) => {
+  return (
+    <div className="w-fit px-2 py-1 rounded bg-indigo-500 bg-opacity-20">
+      <p className="text-indigo-500 text-sm">{language}</p>
+    </div>
+  )
 }
+
+export default Pill
