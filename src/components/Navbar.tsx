50ace6eb94cac4c90fc89fa66538c2683f594d1d
Camila Martinez <camilamarb98@gmail.com>
2024-03-25 10:06:37 +0100

    refactor: move links as a reusable component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,26 +2,25 @@ import { Link } from 'react-router-dom'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiCode } from 'react-icons/hi'
 import { BsMusicNote } from 'react-icons/bs'
+import NavigationLink from './NavigationLink'
+
+const links = [
+  { to: 'https://github.com/cammarb', icon: FaGithub },
+  { to: 'https://linkedin.com/in/cammarb', icon: FaLinkedin },
+  { to: 'music', icon: BsMusicNote },
+]
 
 export default function Navbar() {
   return (
-    <>
-      <nav className="py-3 mb-4 flex justify-between items-center">
-        <Link to={'/'} className="flex items-center gap-1">
-          <HiCode className="text-3xl" />
-        </Link>
-        <div className="flex gap-6">
-          <Link to={'https://github.com/cammarb'}>
-            <FaGithub className="text-neutral-50 text-xl" />
-          </Link>
-          <Link to={'https://linkedin.com/cammarb'}>
-            <FaLinkedin className="text-neutral-50 text-xl" />
-          </Link>
-          <Link to={'music'}>
-            <BsMusicNote className="text-neutral-50 text-xl" />
-          </Link>
-        </div>
-      </nav>
-    </>
+    <nav className="py-3 mb-4 flex justify-between items-center">
+      <Link to={'/'} className="flex items-center gap-1">
+        <HiCode className="text-3xl" />
+      </Link>
+      <div className="flex gap-6">
+        {links.map((link) => (
+          <NavigationLink to={link.to} icon={link.icon}/>
+        ))}
+      </div>
+    </nav>
   )
 }
